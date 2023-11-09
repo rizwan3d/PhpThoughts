f34e58c42927991715df7db37db12d1045580f4b
Muhammad Rizwan <allgame45@hotmail.com>
2023-11-09 16:21:59 +0300

    Add: colorize for CLI command

--- Framework/Cli/Command.php
+++ Framework/Cli/Command.php
@@ -31,4 +31,20 @@ class Command implements CommandInterface
     public function run(): void
     {
     }
+
+    /**
+     * Color output text for the CLI
+     *
+     * @param string $text to color
+     * @param string $color of text
+     * @param string $background color
+     */
+    function colorize($text, $color, $bold = FALSE)
+    {
+        // Standard CLI colors
+        $colors = array_flip(array(30 => 'gray', 'red', 'green', 'yellow', 'blue', 'purple', 'cyan', 'white', 'black'));
+
+        // Escape string with color information
+        return "\033[" . ($bold ? '1' : '0') . ';' . $colors[$color] . "m$text\033[0m";
+    }
 }
